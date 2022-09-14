5c5ad85c4bec19e6fd9ca571c1fa688f78165ed8
Carolyn Webster Pless <carolyn.webster-pless@stu.ocsb.ca>
2022-09-14 18:23:41 +0000

    Told kaitlin more about pythons

---
+++ helloWorldUpdated.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Kaitlin\n";
+    std::cout << "Did you ALSO know that Many Python species, such as P. regius, P. brongersmai, P. bivittatus and M. reticulatus, are popular to keep as pets due to their ease of care, docile temperament, and vibrant colors, with some rare mutations having been sold for several thousands of dollars??? Pythons in the pet trade are sourced from the wild, or from captive females that were taken from the wild ('captive-born'), or from parents that themselves were born in a captive setting ('captive-bred')??? And that despite controversy that has arisen from media reports, with proper safety procedures pet pythons are relatively safe to own??!!\n";
+}
